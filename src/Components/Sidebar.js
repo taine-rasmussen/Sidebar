5451b9e71fa555f86777a02d9ec2a33eb787ac91
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-02-20 17:24:43 +1300

    set up files

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css';
+
+
+function Sidebar() {
+   return (
+      <div className="sidebar">
+      </div>
+   )
+}
+export default Sidebar;
---
+++ src/Components/SidebarData.js
@@ -0,0 +1,7 @@
+export const SidebarData = [
+  {
+    title: 'Home',
+    icon: <Icon />,
+    link: '/Home'
+  }
+]
