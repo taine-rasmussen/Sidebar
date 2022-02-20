27553d3c174df7d87e23d3b404d9a6a0489e28aa
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-02-20 17:34:38 +1300

    set up Sidebar data

--- src/Components/SidebarData.js
+++ src/Components/SidebarData.js
@@ -1,7 +1,39 @@
+import HomeIcon from '@mui/icons-material/Home';
+import MailIcon from '@mui/icons-material/Mail';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import GroupIcon from '@mui/icons-material/Group';
+import ImageIcon from '@mui/icons-material/Image';
+
 export const SidebarData = [
   {
     title: 'Home',
-    icon: <Icon />,
-    link: '/Home'
+    icon: <HomeIcon />,
+    link: '/home'
+  },
+  {
+    title: 'Mail',
+    icon: <MailIcon />,
+    link: '/mail'
+  },
+  {
+    title: 'Analytics',
+    icon: <AssessmentIcon />,
+    link: '/analytics'
+  },
+  {
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+    link: '/dashboard'
+  },
+  {
+    title: 'Friends',
+    icon: <GroupIcon />,
+    link: '/friends'
+  },
+  {
+    title: 'Images',
+    icon: <ImageIcon />,
+    link: '/images'
   }
 ]
